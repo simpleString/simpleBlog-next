a93733c759cc7196c65cb31df1a3cf48cbd6d728
Volkov Dmitry <vdmitry.log@gmail.com>
2023-02-12 21:44:45 +0700

    Fix build bugs.

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -1,24 +1,10 @@
-import Document from "@tiptap/extension-document";
-import Highlight from "@tiptap/extension-highlight";
-import ImageTipTap from "@tiptap/extension-image";
-import Placeholder from "@tiptap/extension-placeholder";
-import Typography from "@tiptap/extension-typography";
-import { EditorContent, useEditor } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
 import { useSession } from "next-auth/react";
-import Image from "next/image";
-import NextLink from "next/link";
 import { useRouter } from "next/router";
-import { FormEvent, ReactElement, useEffect, useState } from "react";
-import { MenuBar } from "../components/editor/MenuBar";
+import { ReactElement } from "react";
 import PostEditor from "../components/editor/PostEditor";
 import LoadingSpinner from "../components/LoadingSpinner";
-import { CloseIcon } from "../components/Svg";
-import { SupabaseBackets } from "../constants/supabase";
 import { Layout } from "../layouts/Layout";
-import { fileUploader } from "../utils/fileUploader";
 import { trpc } from "../utils/trpc";
-import updatePost from "./update-post";
 import type { NextPageWithLayout } from "./_app";
 
 const CreatePost: NextPageWithLayout<React.FC> = () => {
